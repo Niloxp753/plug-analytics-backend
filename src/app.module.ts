a2b2e66351aba0abde8088646540ef84d0629391
Niloxp753 <niloxp.753@gmail.com>
2024-11-13 21:57:16 -0300

    fix: :bug: "Moved PrismaService from UsersModule to AppModule and imported PrismaModule in UsersModule"

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { GraphqlModule } from './graphql/graphql.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [UsersModule, GraphqlModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  providers: [UsersResolver, UsersService, PrismaService],
+  imports: [PrismaModule],
+  providers: [UsersResolver, UsersService],
 })
 export class UsersModule {}
