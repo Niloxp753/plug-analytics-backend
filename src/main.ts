c1a02e28d339b012ae98be00573ff6dd1c7c6955
Niloxp753 <niloxp.753@gmail.com>
2024-11-13 21:48:08 -0300

    feat: :sparkles: "Added cors dependency and configured CORS in main.ts, updated package-lock.json and package.json accordingly"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const prismaService = app.get(PrismaService);
+    await prismaService.onModuleInit();
+
+    const corsOptions: CorsOptions = {
+      origin: ['https://lhg-analytics.vercel.app/'],
+      methods: ['POST', 'OPTIONS'],
+      credentials: true,
+    };
+
+    app.enableCors(corsOptions);
+
+    const port = process.env.PORT || 3000;
+
+    await app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error('Error during application bootstrap:', error);
+  }
 }
 bootstrap();
